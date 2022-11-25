521a3d87e04dd3add789d5074fb11eca32fd2efb
Vishnutha <75074573+Vishnutha@users.noreply.github.com>
2022-11-25 10:54:25 +0530

    Update Prog1.py

--- Prog1.py
+++ Prog1.py
@@ -3,3 +3,11 @@
 
 def summation(data):
     return sum(data)
+def subtraction(a,b):
+    return abs(a-b)
+def division (a,b):
+    return a/b
+def addition(a,b):
+    return a+b
+def multiplication(a,b):
+    return a*b
